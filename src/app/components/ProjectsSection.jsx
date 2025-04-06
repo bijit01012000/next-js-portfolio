f4573f7a5b775545a42cec777084b147a89a6c1a
bijit sahu <bijitsahu225@gmail.com>
2025-04-06 17:39:43 +0530

    customize the portfolio app with the new projects

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -7,19 +7,19 @@ import { motion, useInView } from "framer-motion";
 const projectsData = [
   {
     id: 1,
-    title: "React Portfolio Website",
+    title: "JobTracking APP",
     description: "Project 1 description",
-    image: "/images/projects/1.png",
-    tag: ["All", "Web"],
+    image: "/images/projects/jobTrackingApp.png",
+    tag: ["All", "MERN"],
     gitUrl: "/",
     previewUrl: "/",
   },
   {
     id: 2,
-    title: "Potography Portfolio Website",
+    title: "OpenAI API",
     description: "Project 2 description",
-    image: "/images/projects/2.png",
-    tag: ["All", "Web"],
+    image: "/images/projects/openAI.png",
+    tag: ["All", "MERN"],
     gitUrl: "/",
     previewUrl: "/",
   },
@@ -27,35 +27,80 @@ const projectsData = [
     id: 3,
     title: "E-commerce Application",
     description: "Project 3 description",
-    image: "/images/projects/3.png",
-    tag: ["All", "Web"],
+    image: "/images/projects/ecommerce.png",
+    tag: ["All", "MERN"],
     gitUrl: "/",
     previewUrl: "/",
   },
   {
     id: 4,
-    title: "Food Ordering Application",
+    title: "Cart",
     description: "Project 4 description",
-    image: "/images/projects/4.png",
-    tag: ["All", "Mobile"],
+    image: "/images/projects/cart.png",
+    tag: ["All", "REDUX"],
     gitUrl: "/",
     previewUrl: "/",
   },
   {
     id: 5,
-    title: "React Firebase Template",
+    title: "GITHUB USER",
     description: "Authentication and CRUD operations",
-    image: "/images/projects/5.png",
-    tag: ["All", "Web"],
+    image: "/images/projects/githubUser.png",
+    tag: ["All", "REACT"],
     gitUrl: "/",
     previewUrl: "/",
   },
   {
     id: 6,
-    title: "Full-stack Roadmap",
+    title: "Tour Guide",
     description: "Project 5 description",
-    image: "/images/projects/6.png",
-    tag: ["All", "Web"],
+    image: "/images/projects/TourGuide.png",
+    tag: ["All", "REACT"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+  {
+    id: 7,
+    title: "MENU",
+    description: "Project 5 description",
+    image: "/images/projects/menu.png",
+    tag: ["All", "REACT"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+  {
+    id: 8,
+    title: "SLIDER",
+    description: "Project 5 description",
+    image: "/images/projects/silder.png",
+    tag: ["All", "HTML/CSS"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+  {
+    id: 9,
+    title: "ACCORDION",
+    description: "Project 5 description",
+    image: "/images/projects/accordion.png",
+    tag: ["All", "HTML/CSS"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+  {
+    id: 10,
+    title: "BIRTHDAY BUDDY",
+    description: "Project 5 description",
+    image: "/images/projects/birthdayBuddy.png",
+    tag: ["All", "HTML/CSS"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+  {
+    id: 11,
+    title: "TABS",
+    description: "Project 5 description",
+    image: "/images/projects/tabs.png",
+    tag: ["All", "HTML/CSS"],
     gitUrl: "/",
     previewUrl: "/",
   },
@@ -92,13 +137,23 @@ const ProjectsSection = () => {
         />
         <ProjectTag
           onClick={handleTagChange}
-          name="Web"
-          isSelected={tag === "Web"}
+          name="HTML/CSS"
+          isSelected={tag === "HTML/CSS"}
         />
         <ProjectTag
           onClick={handleTagChange}
-          name="Mobile"
-          isSelected={tag === "Mobile"}
+          name="REACT"
+          isSelected={tag === "REACT"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="REDUX"
+          isSelected={tag === "REDUX"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="MERN"
+          isSelected={tag === "MERN"}
         />
       </div>
       <ul ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12">
